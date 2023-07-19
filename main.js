1911136d9556855f52fbe7272e8468da5d1ca5f9
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-19 15:52:28 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,8 @@
+var arr=[11,10,70,59,2,9];
+document.write("Given array is "+[11,10,70,59,2,9]+"<br>");
+for(let i=0; i<arr.length; i++){
+								if(arr[i]>9&&arr[i]<=99)
+								{
+								document.write(arr[i]+"<br>");
+								}
+								}
